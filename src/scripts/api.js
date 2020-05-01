34c2d4367fa225dcfc18bb1ce8fbd57c2225c037
Victor Jarvis <vctrjrvs@gmail.com>
2020-05-01 14:11:01 -0400

    git and npm issues resolved

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -2,16 +2,66 @@
 
 const BASE_URL = 'https://thinkful-list-api.herokuapp.com/victor';
 
-
-
-
-
-
-deleteBookmark{
-     method: 'DELETE'
+const listOfFetch = function (...arguments) {
+     let error;
+     return fetch(...arguments)
+          .then(response => {
+               if (!response.ok) {
+                    error = {
+                         code: response.status
+                    };
+                    if (response.headers.get('content-type').includes('json')) {
+                         error.message = response.statusText;
+                         return Promise.reject(error);
+                    }
+               }
+               return res.json();
+          })
+          .then(data => {
+               if (error) {
+                    error.message = data.message;
+                    return Promise.reject(error);
+               }
+               return data;
+          });
 }
 
-export default { 
+getBookmarks = function () {
+     return listOfFetch(`${BASE_URL}/bookmarks`);
+}
+
+createBookmark = function (title) {
+     let newBookmarkInfo = JSON.stringify({
+          title
+     })
+     return listOfFetch(`${BASE_URL}/bookmarks`, {
+          method: 'POST',
+          headers: {
+               'Content-Type': 'application/json',
+          },
+          body: newBookmark
+     });
+}
+
+editBookmark = function (id, updateData) {
+     let newBookmarkData = JSON.stringify(updateData)
+     return listOfFetch(`${BASE_URL}/bookmarks`, {
+          method: 'PATCH',
+          headers: {
+               'Content-Type': 'application/json',
+          },
+          body: newBookmarkData
+     });
+}
+
+
+deleteBookmark = function (id) {
+     return listOfFetch(`${BASE_URL}/bookmarks`, {
+          method: 'PATCH'
+     });
+}
+
+export default {
      createBookmark,
      getBookmarks,
      editBookmark,
